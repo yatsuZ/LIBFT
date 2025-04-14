b8ea8f023f60c691a5c0abbd4f22974bb2f884c5
yatsuZ <yassine.zaoui28062003@gmail.com>
2025-04-14 15:37:16 +0200

    is digit fais + ajoue du git igonre

--- code/LIBFT/part_1/ft_isdigit.c
+++ code/LIBFT/part_1/ft_isdigit.c
@@ -6,15 +6,19 @@
 /*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:20:35 by yzaoui            #+#    #+#             */
-/*   Updated: 2025/04/14 15:24:09 by yzaoui           ###   ########.fr       */
+/*   Updated: 2025/04/14 15:29:24 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/// @brief Vérifie si le paramètre est un caractère alphabétique.
+#include "./../libft.h"
+
+/// @brief Vérifie si le paramètre est un caractère numeric base 10.
 /// @param c Le paramètre à vérifier (int).
-/// @return 1 si c'est un caractère alphabétique, 0 sinon.
-int	ft_isalpha(int	c)
+/// @return 1 si c'est un caractère numeric, 0 sinon.
+int	ft_isdigit(int c)
 {
-	unsigned char newc = c;
-	return ((newc >= 'A' && newc <= 'Z') || (newc >= 'a' && newc <= 'z'));
+	unsigned char	newc;
+
+	newc = c;
+	return ((newc >= '0' && newc <= '9'));
 }
